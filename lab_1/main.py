d5fcaa411e00fff41f9b80f391ccd86a0806b20b
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2020-03-29 18:15:36 +0300

    Outcomes fixed

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,29 +1,37 @@
-#!/bin/python
+#!/usr/bin/python
 
 import csv
 
-class tarificator(
+class tarificator():
 
-    # Defaults
+    def __init__(
+            self,
+            # Defaults
+            outcome_limit = 0,
+            income_limit = 50,
+            k_income_prelimit = 1,
+            k_outcome_prelimit = 1,
+            k_income_postlimit = 1,
+            k_outcome_postlimit = 1,
+            free_sms_count = 5,
+            data_file_path = './data.csv'
+        ):
 
-    outcome_limit = 0,
-    income_limit = 50,
-    k_income_prelimit = 1,
-    k_outcome_prelimit = 1,
-    k_income_postlimit = 1,
-    k_outcome_postlimit = 1,
-    free_sms_count = 5,
-    data_file_path = './data.csv'
+        # Initialise variables
 
-):
-
-    def __init__(self):
+        self.outcome_limit = outcome_limit
+        self.income_limit = income_limit
+        self.k_income_prelimit = k_income_prelimit
+        self.k_outcome_prelimit = k_outcome_prelimit
+        self.k_income_postlimit = k_income_postlimit
+        self.k_outcome_postlimit = k_outcome_postlimit
+        self.free_sms_count = free_sms_count
+        self.data_file_path = data_file_path
 
         # Collect data from file
 
         data_file = open(self.data_file_path, 'r')
-        self.reader = csv.DictReader(data_file, delimeter=',')
-        data_file.close()
+        self.reader = csv.DictReader(data_file, delimiter=',')
 
     def tarificate_tel(self, target):
 
@@ -34,19 +42,22 @@ class tarificator(
         income_limit = self.income_limit
 
         for line in self.reader:
+            call_duration = float(line['call_duration'])
+
             if ( line['msisdn_origin'] == target ):
-                if ( outcome_limit - line['call_duration'] > 0 ):
-                    outcome_limit -= line['call_duration']
-                    cost += line['call_duration'] * self.k_outcome_prelimit
+                if ( outcome_limit - call_duration > 0 ):
+                    outcome_limit -= call_duration
+                    cost += call_duration * self.k_outcome_prelimit
                 else:
-                    outcome_limit -= line['call_duration']
-                    cost += (0 - outcome_limit)*self.k_outcome_postlimit + (line['call_duration'] + outcome_limit)*self.k_outcome_prelimit
+                    outcome_limit -= call_duration
+                    cost += (0 - outcome_limit)*self.k_outcome_postlimit + (call_duration + outcome_limit)*self.k_outcome_prelimit
                     outcome_limit = 0
+
             if ( line['msisdn_dest'] == target ):
                 a = 0
-                # cost += line['call_duration']
+                # cost += line['call_duration'])
 
-        print(cost)
+        return cost
 
     def tarificate_sms(self):
         print('sms')
@@ -56,4 +67,4 @@ if __name__ == '__main__':
     # Init work class
 
     counter = tarificator()
-    counter.tarificate_tel('911926375')
+    print(counter.tarificate_tel('911926375'))
